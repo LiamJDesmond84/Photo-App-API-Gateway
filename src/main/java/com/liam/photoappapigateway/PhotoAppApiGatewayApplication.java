64bd68d55b9f6c9c047b12fbfcbc22deebf97173
Liam Desmond <liamjdesmond84@gmail.com>
2022-04-30 19:57:43 -0500

    inital commit

---
+++ src/main/java/com/liam/photoappapigateway/PhotoAppApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.liam.photoappapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppApiGatewayApplication.class, args);
+	}
+
+}
